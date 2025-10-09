7578cb11efa4f8ea34d2ca655f45a15590e163e3
Maxima <MaximaRasskazov@gmail.com>
2025-10-09 11:49:31 +0500

    chore: Добавлен комментарий для запуска

--- start_auth.sh
+++ start_auth.sh
@@ -12,3 +12,5 @@ cd auth && \
 
 
 # cd auth && set PORT=9100 && set HOST=localhost set SERVER_TIMEOUT=10 set DB_TIMEOUT=5 set JWT_SECRET_KEY=secret_key  set JWT_ACCESS_TOKEN_EXPIRATION=24 set JWT_REFRESH_TOKEN_EXPIRATION=168   && go run main.go
+
+#cd auth && set PORT=8101&& set HOST=localhost&& set SERVER_TIMEOUT=10&& set DB_TIMEOUT=5&& set JWT_SECRET_KEY=secret_key&& set JWT_ACCESS_TOKEN_EXPIRATION=24&& set JWT_REFRESH_TOKEN_EXPIRATION=168&& go run main.go
