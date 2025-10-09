9e4a637a1a0e9993d9220779fd4180bb7acabef2
Maxima <MaximaRasskazov@gmail.com>
2025-10-09 12:18:09 +0500

    feat: Создал обработчики

---
+++ auth/internal/handler/handler.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"auth/internal/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Handler содержит все обработчики для работы с пользователями
+type Handler struct {
+	cfg *config.Config
+}
+
+func NewHandler(cfg *config.Config) *Handler {
+	return &Handler{
+		cfg: cfg,
+	}
+}
+
+func (h Handler) RegisterUser(c *gin.Context) {
+	c.JSON(201, gin.H{
+		"message": "Пользователь успешно зарегистрирован",
+	})
+}
+
+func (h Handler) LoginUser(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message":       "Пользователь успешнр авторизован",
+		"access_token":  "example_access_token",
+		"refresh_token": "example_refresh_token",
+	})
+}
+
+func (h Handler) GetInfoUser(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Информация о пользователе получена",
+	})
+}
+
+func (h Handler) UpdateUser(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Данные пользователья обновлены",
+	})
+}
+
+func (h Handler) DeleteUser(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Пользователь успешно удален",
+	})
+}
