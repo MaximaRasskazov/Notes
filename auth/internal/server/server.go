e028925a811667bb40feddce5e5ad10b0fc32284
Maxima <MaximaRasskazov@gmail.com>
2025-10-10 17:46:40 +0500

    feat: Добавил на Сервер Маршрутизатор

--- auth/internal/server/server.go
+++ auth/internal/server/server.go
@@ -3,33 +3,46 @@ package server
 import (
 	"auth/internal/config"
 	"auth/internal/handler"
+	"auth/internal/routes"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
-// У серва должны быть
-// Конфигурации
-// Обработчики
+// У сервера должны быть:
+// 	1. Конфигурации
+// 	2. Обработчики
 
+// Структура сервера
 type Server struct {
+	// Конфиг (Порт, хост и тд)
 	cfg *config.Config
+
+	// Маршрутизатор ("/auth/register" вызывает RegisterUser)
+	router *gin.Engine
 }
 
+// Конструктор сервера
 func NewServer(cfg *config.Config) (*Server, error) {
 	// Проверим, что конфиг не пустой
 	if cfg == nil {
-		return nil, fmt.Errorf("Конфигурация сервера не может быть nil")
+		return nil, fmt.Errorf("конфигурация сервера не может быть nil")
 	}
 
 	// Новый экземпляр обработчика
 	handler := handler.NewHandler(cfg)
 	if handler == nil {
-		return nil, fmt.Errorf("Не удалось создать обработчик сервера")
+		return nil, fmt.Errorf("не удалось создать обработчик сервера")
 	}
 	fmt.Println("Обработчика сервера успешно создан")
 
-	// Раз все окей, то создадим сервер
+	// Создаем экземпляр маршрутизатора
+	router := routes.SetupRouter(handler)
+
+	// Раз всё окей, то создадим сервер
 	return &Server{
-		cfg: cfg,
+		router: router,
+		cfg:    cfg,
 	}, nil
 }
 
@@ -37,5 +50,12 @@ func (s *Server) Serve() error {
 	// Запускаем Сервер
 	address := fmt.Sprintf("%s:%s", s.cfg.Host, s.cfg.Port)
 	fmt.Printf("Сервер запущен на %s\n", address)
+
+	return s.router.Run(address)
+}
+
+// Stop - остановка сервера
+func (s *Server) Stop() error {
+	fmt.Println("Сервер остановлен")
 	return nil
 }
