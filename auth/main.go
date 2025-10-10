dd5f3a8156d3bf34176efc1390c23010398eff9b
Maxima <MaximaRasskazov@gmail.com>
2025-10-10 18:37:39 +0500

    docs: Закомментировал вывод данных среды

--- auth/main.go
+++ auth/main.go
@@ -10,16 +10,16 @@ func main() {
 	// Инициализируем конфигурации сервера
 	cfg := config.NewConfig()
 
-	fmt.Printf("=== Server Configuration ===\n")
-	fmt.Printf("Host: %s\n", cfg.Host)
-	fmt.Printf("Port: %s\n", cfg.Port)
-	fmt.Printf("Database URL: %s\n", cfg.DBDSN)
-	fmt.Printf("Access Token Expiration: %d hours\n", cfg.AccessTokenExpiration)
-	fmt.Printf("Refresh Token Expiration: %d hours\n", cfg.RefreshTokenExpiration)
-	fmt.Printf("Server Timeout: %d seconds\n", cfg.Timeout)
-	fmt.Printf("Database Timeout: %d seconds\n", cfg.DBTimeout)
-	fmt.Printf("JWT Secret Key: %s\n", cfg.JWTSecretKey)
-	fmt.Printf("=============================\n")
+	// fmt.Printf("=== Server Configuration ===\n")
+	// fmt.Printf("Host: %s\n", cfg.Host)
+	// fmt.Printf("Port: %s\n", cfg.Port)
+	// fmt.Printf("Database URL: %s\n", cfg.DBDSN)
+	// fmt.Printf("Access Token Expiration: %d hours\n", cfg.AccessTokenExpiration)
+	// fmt.Printf("Refresh Token Expiration: %d hours\n", cfg.RefreshTokenExpiration)
+	// fmt.Printf("Server Timeout: %d seconds\n", cfg.Timeout)
+	// fmt.Printf("Database Timeout: %d seconds\n", cfg.DBTimeout)
+	// fmt.Printf("JWT Secret Key: %s\n", cfg.JWTSecretKey)
+	// fmt.Printf("=============================\n")
 
 	server, err := server.NewServer(cfg)
 	if err != nil {
@@ -32,5 +32,5 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Сервер успешно запущен\n")
+	fmt.Printf("\nСервер успешно запущен\n")
 }
